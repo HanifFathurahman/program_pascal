7aa3b1e3f7cdf73734319383199cf82daf70d972
Hanif Fathurahman <58112881+HanifFathurahman@users.noreply.github.com>
2022-10-07 11:16:16 +0700

    Update Hitung Bangun Datar.pas

--- Hitung Bangun Datar.pas
+++ Hitung Bangun Datar.pas
@@ -8,15 +8,18 @@ p2,l2,hasil2:integer;
 a,t2:integer;
 hasil3,hasil4:real;
 r:real;
+aj,tj,hasil5:integer;
+
 
 begin
 clrscr;
-	delay(100); writeln('|------------------------|');
-	delay(100); writeln('|    1. Volume Balok     |');
-	delay(100); writeln('|    2. Volume Kubus     |');
-	delay(100); writeln('|    3. Luas Segitiga    |');
-	delay(100); writeln('|    4. Luas lingkaran   |');
-	delay(100); writeln('|-----=------------------|');
+	delay(100); writeln('|-------------------------------|');
+	delay(100); writeln('|      1. Volume Balok          |');
+	delay(100); writeln('|      2. Volume Kubus          |');
+	delay(100); writeln('|      3. Luas Segitiga         |');
+	delay(100); writeln('|      4. Luas lingkaran        |');
+	delay(100); writeln('|      5. Luas Jajar Genjang    |');
+	delay(100); writeln('|-----=-------------------------|');
 	delay(100); write('Silahkan di pilih : ');
 	delay(100); readln(pilih);
 	writeln;
@@ -74,5 +77,18 @@ if (pilih=4) then
 	delay(100);hasil4:=pi*r*r;
 	delay(100);writeln('Hasilnya adalah : ',hasil4:1:2);
 	delay(100);readln;
+	end
+
+//LUAS JAJAR GENJANG
+else
+if(pilih=5) then
+	begin
+	delay(100);write('Masukkan Alas Jajar Genjang   : ');
+	delay(100);readln(aj);
+	delay(100);write('Masukkan Tinggi Jajar Genjang : ');
+	delay(100);readln(tj);
+	delay(100);hasil5:= 2 * (aj + tj);
+	delay(100);writeln('Hasilnya adalah : ',hasil5);
+	delay(100);readln(hasil5);
 	end;
 end.
